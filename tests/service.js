bcf5691813a6d80ebb42270d7aef3012ee359895
Markus Felten <Markus.Felten@gmx.de>
2015-12-18 19:14:40 +0100

    fist steps to implement consul as a kronos service

---
+++ tests/service.js
@@ -0,0 +1,53 @@
+/* global describe, it*/
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should();
+chai.use(require('chai-as-promised'));
+
+const http = require('http');
+
+const kronos = require('kronos-service-manager'),
+  service = require('../lib/consulService.js');
+
+const srv = http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('okay');
+});
+
+srv.listen(8500, '127.0.0.1');
+
+describe('consul service', function () {
+  it("create", function (done) {
+    kronos.manager().then(
+      function (manager) {
+        describe('create', function () {
+          try {
+            console.log("AA");
+
+            service.registerWithManager(manager);
+            const cs = manager.serviceGet('consul');
+
+            assert.equal(cs.name, "consul");
+            assert.equal(cs.state, "stopped");
+
+            cs.start().then(f => {
+              assert.equal(cs.state, "running");
+              done();
+            }, done);
+            //done();
+          } catch (err) {
+            done(err);
+          }
+        });
+      }
+    );
+  });
+
+});
