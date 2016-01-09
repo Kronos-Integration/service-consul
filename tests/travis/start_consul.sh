a47aa09f380c9a4b323192c054b395cdb5625cfd
Markus Felten <Markus.Felten@gmx.de>
2016-01-09 16:24:15 +0100

    chore(travis): trying to download and start consul agent

---
+++ tests/travis/start_consul.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+pwd
+
+NAME=consul
+VERSION=0.6.1
+ARCHIVE_NAME=${NAME}_${VERSION}_linux_amd64.zip
+
+if [ ! -d "$DIR/$NAME" ]; then
+  curl -O -insecure https://releases.hashicorp.com/${NAME}/${VERSION}/${ARCHIVE_NAME}
+  unzip ${ARCHIVE_NAME}
+fi
+
+./consul version
+
+nohup ./consul agent -dev &
